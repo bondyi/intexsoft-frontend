4990fc200426a022b2c29795d93ad07fef60adc9
Ilya Bondarik <il.bandaryk@gmail.com>
2022-10-27 02:49:08 +0300

    added alert with info from input elements

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import './app.css';
 
 const app = () => {
-    return (<div className="app"/>);
+    const handleClick = () => {
+        let login = document.getElementById('input-login').value.toString();
+        let password = document.getElementById('input-password').value.toString();
+
+        let JSONObject = {
+            login: login,
+            password: password
+        };
+
+        alert(JSON.stringify(JSONObject));
+    }
+
+    return (
+        <div className="app">
+            <input type="text" id="input-login"/>
+            <input type="text" id="input-password"/>
+            <button onClick={handleClick}>
+                Say in JSON
+            </button>
+        </div>
+    );
 }
 
 export default app;
